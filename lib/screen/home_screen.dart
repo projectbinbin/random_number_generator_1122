8d8c373e8ebf65a8f8f4dcd63f355d0c4fb0ab3f
projectbinbin <91408611+projectbinbin@users.noreply.github.com>
2022-11-22 19:05:15 +0900

    난수생성하기(완료)

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:random_number_generator1121/constant/color.dart';
 
@@ -9,6 +11,12 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<int> randomNumbers = [
+    123,
+    456,
+    789,
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,7 +52,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [123, 456, 789]
+                      children: randomNumbers
                           .asMap()
                           .entries
                           .map(
@@ -75,7 +83,21 @@ class _HomeScreenState extends State<HomeScreen> {
                   style: ElevatedButton.styleFrom(
                     backgroundColor: RED_COLOR,
                   ),
-                  onPressed: () {},
+                  onPressed: () {
+                    final rand = Random();
+                    final Set<int> newNumbers = {};
+
+                    while (newNumbers.length < 3)
+                    {
+                      final number = rand.nextInt(1000);
+                      newNumbers.add(number);
+                    }
+
+                    setState(() {
+                      randomNumbers = newNumbers.toList();
+                    });
+                    print(randomNumbers);
+                  },
                   child: Text('생성하기'),
                 ),
               ),
