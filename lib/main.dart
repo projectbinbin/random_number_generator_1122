bcea72f49a108f5bb4c9a19370cb120fcdc7fb42
projectbinbin <91408611+projectbinbin@users.noreply.github.com>
2022-11-22 12:31:50 +0900

    디자인적용하기(처리중)

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:random_number_generator1121/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
